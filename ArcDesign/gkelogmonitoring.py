91f8f5e966332a1b054f7dfdf86ed9c34b5c6edf
Khalid I <90557979+KhalidIbra@users.noreply.github.com>
2025-09-12 18:21:32 +0100

    Update gkelogmonitoring.py

--- ArcDesign/gkelogmonitoring.py
+++ ArcDesign/gkelogmonitoring.py
@@ -2,28 +2,24 @@ import time
 from datetime import datetime, timedelta
 from google.cloud import logging
 
-# Defines keywords to search for in the logs
+
 KEYWORDS = ["ERROR", "FAIL"]
 
-# Initializes Google Cloud Logging client
+
 client = logging.Client()
 
 GKE_CLUSTER_NAME = "your-gke-cluster-name"
 GKE_NAMESPACE = "your-namespace"
 GKE_PROJECT_ID = "your-gke-project-id"
-GKE_LOCATION = "your-cluster-region"  # e.g., "us-central1"
+GKE_LOCATION = "your-cluster-region"  
 
-# Function to fetch logs from Google Cloud Logging
 def fetch_logs():
-    # Get the current time and calculate the time 10 minutes ago
     current_time = datetime.utcnow()
     time_10_minutes_ago = current_time - timedelta(minutes=10)
 
-    # Convert the timestamps to RFC3339 format (requirement for by Google Cloud Logging API)
     current_time_rfc3339 = current_time.isoformat("T") + "Z"
     time_10_minutes_ago_rfc3339 = time_10_minutes_ago.isoformat("T") + "Z"
 
-    # Constructs the filter to search logs for the last 10 minutes, for GKE container logs
     filter_str = f"""
         resource.type="k8s_container"
         resource.labels.cluster_name="{GKE_CLUSTER_NAME}"
